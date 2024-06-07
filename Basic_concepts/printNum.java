25753951f52b7c9c0c5ccc403318db4a34cbb197
raihanuldev <rihanthedev@gmail.com>
2024-06-07 07:39:04 +0600

    printNum

---
+++ Basic_concepts/printNum.java
@@ -0,0 +1,8 @@
+public class printNum {
+    public static void main(String[] args){
+        System.out.println(3);
+        System.out.println(3*7);
+        System.out.println(3*7+8);
+        System.out.println(3*7-8);
+    }
+}
