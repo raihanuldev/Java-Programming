abcdb950b38e94b8393399df89ff548260631e6c
raihanuldev <rihanthedev@gmail.com>
2024-06-07 07:29:34 +0600

    got it

--- Basic_concepts/info.java
+++ Basic_concepts/info.java
@@ -1,5 +1,7 @@
 public class info {
     public static void main(String[] args){
+        System.out.print(123);
+        System.out.println("Hello Bro");
         System.out.println("HI Iam Raihan Sharif. Iam a Student of Maheskahli cox's bazar. you know that iam a software Enginner and i have a big drem for my life. thank you so much my god for give me this life");
     }
 }
