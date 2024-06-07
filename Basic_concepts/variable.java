b5c147df93f7f0ef05f4132172a9b06b6d292806
raihanuldev <rihanthedev@gmail.com>
2024-06-07 07:49:05 +0600

    varibales

---
+++ Basic_concepts/variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args){
+        int myRoll = 789145;
+        String Name = "Raihan sharif";
+        System.out.println(Name);
+        System.out.println(myRoll);
+    }
+}
