9060fc668624cb6328deb7689acc8c76b428a38f
raihanuldev <rihanthedev@gmail.com>
2024-06-07 07:43:11 +0600

    multiline

--- Basic_concepts/comments.java
+++ Basic_concepts/comments.java
@@ -2,5 +2,9 @@ public class comments {
     public static void main(String[] args) {
         System.out.println("This is Comment Line on Java");
         // System.out.println(" There are commenting are good for ");
+        // multipline Comments on here
+        /*
+        hello world this is multiline Code Commenting for programm
+        */ 
     }
 }
