9faba49230f152426fbd05df2020d608d7e94379
raihanuldev <rihanthedev@gmail.com>
2024-06-07 07:36:04 +0600

    got it

---
+++ Basic_concepts/print.java
@@ -0,0 +1,8 @@
+public class print {
+    public static void main(String[] args) {
+        System.out.println("Hello broo");
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+    }
+}
