34e0b3703435ad004212c87568af378a412b1358
raihanuldev <rihanthedev@gmail.com>
2024-06-07 07:41:10 +0600

    comments

---
+++ Basic_concepts/comments.java
@@ -0,0 +1,6 @@
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("This is Comment Line on Java");
+        // System.out.println(" There are commenting are good for ");
+    }
+}
