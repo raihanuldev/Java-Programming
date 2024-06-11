9d2e85c068c20225d317b646b77c4b78ffe2ac70
raihanuldev <rihanthedev@gmail.com>
2024-06-11 21:15:52 +0600

    java

---
+++ apnaCollage/lecture_2/qs.java
@@ -0,0 +1,8 @@
+public class qs {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
