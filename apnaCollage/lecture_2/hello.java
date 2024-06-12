2c677d10ab3efa17e04396db9b6119767b883fbf
raihanuldev <rihanthedev@gmail.com>
2024-06-12 13:16:43 +0600

    Boolean

--- apnaCollage/lecture_2/hello.java
+++ apnaCollage/lecture_2/hello.java
@@ -1,6 +1,8 @@
 public class hello {
     public static void main(String[] args) {
+        boolean isTrue = false;
         byte myNum = 100;
         System.out.println(myNum);
+        System.out.println(isTrue);
     }
 }
