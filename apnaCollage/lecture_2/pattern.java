488a71cf03204116eda26e4c89c93887e4af3839
raihanuldev <rihanthedev@gmail.com>
2024-06-12 12:59:56 +0600

    code updated

--- apnaCollage/lecture_2/pattern.java
+++ apnaCollage/lecture_2/pattern.java
@@ -1,5 +1,5 @@
 public class pattern {
     public static void main(String[] args) {
-        System.out.println(("This is Practice Java Programming. "));
+        System.out.println((123+34));
     }
 }
