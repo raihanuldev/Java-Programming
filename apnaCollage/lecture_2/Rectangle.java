a3eef7e7d3befb570c8677f58604d6815dbba081
raihanuldev <rihanthedev@gmail.com>
2024-06-12 13:12:03 +0600

    Rectangle

---
+++ apnaCollage/lecture_2/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        int length = 45;
+        int weight = 78;
+        int area;
+
+        // Calculete area 
+        area = length * weight;
+        System.out.println(area);
+    }
+}
