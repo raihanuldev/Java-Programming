9c2d7cffc209a3cb12112ac840eb4865007678cb
raihanuldev <rihanthedev@gmail.com>
2024-06-12 13:08:54 +0600

    https://www.w3schools.com/java/java_variables.asp

--- apnaCollage/lecture_2/variable.java
+++ apnaCollage/lecture_2/variable.java
@@ -1,6 +1,10 @@
 public class variable {
     public static void main(String[] args) {
         String name = "Raihan Sharif";
+        int age = 20;
+        double weight = 15.4;
+        
+        System.out.println(age+weight);
         System.out.printf("My Name Is: "+ name);
     }
 }
