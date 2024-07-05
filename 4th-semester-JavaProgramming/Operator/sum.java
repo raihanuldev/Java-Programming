a9f352b73242ee31d18cbf7d878890eb70cfa386
raihanuldev <rihanthedev@gmail.com>
2024-07-05 09:04:01 +0600

    uff syntx er maire bap

--- 4th-semester-JavaProgramming/Operator/sum.java
+++ 4th-semester-JavaProgramming/Operator/sum.java
@@ -1,10 +1,13 @@
-/**
- * sum
- */
-public class sum {
-    public static void main(String[] args) {
-        System.out.print("Enter YOur num: ");
-        int a = 
+import java.util.Scanner;
+
+class Sum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tumar Frist Number ta dow: ");
+        int a = sc.nextInt();
+        System.out.print("Enter your secound Number: ");
+        int b = sc.nextInt();
+        int total = a+b;
+        System.out.println("total: "+ total);
     }
-    
 }
