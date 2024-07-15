808abb7050bf81fd94eee68ae44c113bd297ef96
raihanuldev <rihanthedev@gmail.com>
2024-07-15 11:06:12 +0600

    java

---
+++ 4th-semester-JavaProgramming/Operator/exOne.java
@@ -0,0 +1,15 @@
+public class exOne {
+    public static void main(String arg[]){
+        int a = 10;
+        int b = 2;
+        float d;
+        int result;
+        d = a/b;
+        System.out.println(d);
+        if (d >4) {
+            System.out.println("Hello World We are Here to Eat referd");
+        }else{
+            System.out.println("Helllo");
+        }
+    }
+}
