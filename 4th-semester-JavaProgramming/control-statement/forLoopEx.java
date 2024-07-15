69359ac57d144a4a2e1b8512efccb28e5b667ee3
raihanuldev <rihanthedev@gmail.com>
2024-07-15 12:14:58 +0600

    added

---
+++ 4th-semester-JavaProgramming/control-statement/forLoopEx.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class forLoopEx {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter value For N=>:  ");
+        int n = input.nextInt();
+        int sum = 0;
+        for(int i = 1; i<= n; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
