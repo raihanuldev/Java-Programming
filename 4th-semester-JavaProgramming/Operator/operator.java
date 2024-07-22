8dda1f63585e99847b2cc28118a67bfa9dbc67a1
raihanuldev <rihanthedev@gmail.com>
2024-07-22 15:40:36 +0600

    4.2

---
+++ 4th-semester-JavaProgramming/Operator/operator.java
@@ -0,0 +1,7 @@
+public class operator {
+    public static void main(String args[]){
+        int a = 123213;
+        int b = 14;
+        System.out.println(a+b);
+    }
+}
---
+++ 4th-semester-JavaProgramming/control-statement/decisionMake.java
@@ -0,0 +1,8 @@
+public class decisionMake{
+    public static void main(String[] args){
+        int age = 30;
+        if(age>25){
+            System.out.println("Age IS greater then 25");
+        }
+    }
+}
