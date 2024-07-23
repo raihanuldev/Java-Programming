1e115951130628d13fc6eeb31d8c61de768cfa7c
raihanuldev <rihanthedev@gmail.com>
2024-07-23 13:41:02 +0600

    Series

---
+++ 4th-semester-JavaProgramming/control-statement/seriesSum.java
@@ -0,0 +1,16 @@
+/**
+ * seriesSum
+ */
+public class seriesSum {
+
+    public static void main(String[] args) {
+        int total =0;
+        int n = 100;
+
+        for(int i =0; i<=n; i++){
+            total = total+i;
+        }
+        System.out.println("Seris of "+n+" Sum=> "+ total);
+
+    }
+}
