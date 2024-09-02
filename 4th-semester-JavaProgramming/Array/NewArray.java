248c757b78366403b9d0babed48ef30f50844f19
raihanuldev <rihanthedev@gmail.com>
2024-09-02 15:12:24 +0600

    practices

---
+++ 4th-semester-JavaProgramming/Array/NewArray.java
@@ -0,0 +1,11 @@
+
+public class NewArray {
+    public static void main(String[] args) {
+        double[] myList = {1.9,1.3,4.6,2.3};
+
+        // print all the array elements
+        for(int i =0; i<myList.length; i++){
+            System.out.println(myList[i]+" element of myList");
+        }
+    }
+}
--- 4th-semester-JavaProgramming/control-statement/forLoopEx.java
+++ 4th-semester-JavaProgramming/control-statement/forLoopEx.java
@@ -4,7 +4,8 @@ public class forLoopEx {
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter value For N=>:  ");
-        int n = input.nextInt();
+        int n = 10;
+        // int n = input.nextInt();
         int sum = 0;
         for(int i = 1; i<= n; i++){
             sum+=i;
