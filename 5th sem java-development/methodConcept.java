b4811a06f939eb3df3435b9c5f2411f0ba6ef188
raihanuldev <rihanthedev@gmail.com>
2025-06-12 06:49:21 +0600

    Add methodConcept class with mymethod for printing origin messages

---
+++ 5th sem java-development/methodConcept.java
@@ -0,0 +1,11 @@
+public class methodConcept {
+    static void mymethod(String origin){
+        System.out.println("I Just Excute Now! From Origin "+ origin);
+    }
+    public static void main(String[] arg)
+    {
+        mymethod("Frist Line");
+        mymethod("Minister home");
+        mymethod("White House");
+    }
+}
